8f6bb826ff19b8ea6608eda346fb3aeaef710f54
seoyoenkk68 <seoyeonkk68@gmail.com>
2021-07-29 16:05:06 +0900

    gsap작업

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,114 @@
 gsap.from(".img_1", {
   x: 300,
-  ease: Power2.easeIn,
   opacity: 0,
   duration: 1.5,
+})
+
+
+gsap.from(".img_2", {
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_2',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_3", {
+  y: -100,
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_2',
+    markers:true,
+    start: '0, 55%'
+  }
+})
+
+gsap.from(".img_4", {
+  y:200,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_4',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_5", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_5',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_6", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_6',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_7", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_7',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_8", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_8',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_13-16", {
+  y:100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_13-16',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_17", {
+  opacity:0,
+  duration:4,
+  scrollTrigger:{
+    trigger:'.img_17',
+    markers:true,
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_18", {
+  x:100,
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_18',
+    markers:true,
+    start: '0, 40%'
+  }
 })
